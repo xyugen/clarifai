d412345b5fa8de7b03d6225ccb7091ecb983de9b
xyugen <alex.arias.jorym@gmail.com>
2025-11-09 13:34:31 +0800

    feat: update Title component to use Link for navigation to dashboard

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,15 +1,16 @@
 import { Card } from "@/components/retroui/Card";
 import { Text } from "@/components/retroui/Text";
-import React from "react";
+import { PageRoutes } from "@/constants/page-routes";
+import Link from "next/link";
 
 const Title = () => {
   return (
-    <div className="flex items-center gap-3">
+    <Link href={PageRoutes.DASHBOARD} className="flex items-center gap-3">
       <Card className="bg-primary flex size-12 rotate-3 items-center justify-center border-4 shadow-none hover:shadow-none">
         <Text as={"h3"}>C</Text>
       </Card>
       <Text as={"h4"}>ClarifAI</Text>
-    </div>
+    </Link>
   );
 };
 
