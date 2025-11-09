d2555f28394eb2b856840a3e0bb361af9bbdeef2
xyugen <alex.arias.jorym@gmail.com>
2025-11-09 12:10:45 +0800

    Checkpoint from VS Code for coding agent session

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,6 @@
-import { Card } from "@/components/retroui/Card";
 import React from "react";
+import AuthForm from "./_components/auth-form";
 import Branding from "./_components/branding";
-import { Text } from "@/components/retroui/Text";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,19 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Branding />
 
       {/* Right Side - Auth Form */}
-      <section className="w-full max-w-lg lg:3/4">
-        <div className="mb-4 flex items-center justify-center lg:hidden">
-          <Text
-            as="h2"
-            className="mr-4 inline-block -rotate-1 border-4 border-black bg-primary px-4 py-2 font-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]"
-          >
-            ClarifAI
-          </Text>
-        </div>
-        <Card className="w-full p-6 shadow-none hover:shadow-none lg:p-8">
-          {children}
-        </Card>
-      </section>
+      <AuthForm>{children}</AuthForm>
     </div>
   );
 };
