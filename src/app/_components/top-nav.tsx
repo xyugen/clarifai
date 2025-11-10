367e203444ca665cf9eb6e67924282aab39736ce
xyugen <alex.arias.jorym@gmail.com>
2025-11-10 22:34:28 +0800

    feat: add dashboard button in landing for logged in users

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,26 +1,42 @@
 import { Button } from "@/components/retroui/Button";
 import { TextLink } from "@/components/retroui/TextLink";
-import { PageRoutes } from "@/constants/page-routes";
-import Link from "next/link";
 import Title from "@/components/title";
+import { PageRoutes } from "@/constants/page-routes";
+import { auth } from "@/server/better-auth";
+import { headers } from "next/headers";
+import Link from "next/link";
 
-const TopNav = () => {
+const TopNav = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <nav className="border-b-2 border-black bg-white p-4">
       <div className="mx-auto flex max-w-7xl items-center justify-between">
         <Title />
-        <div className="hidden items-center gap-6 md:flex">
-          <TextLink
-            href={PageRoutes.LOGIN}
-            className="font-bold decoration-4 hover:underline"
-          >
-            Login
-          </TextLink>
-          <Link href={PageRoutes.SIGNUP}>
-            <Button variant="default" className="text-sm">
-              Get Started
-            </Button>
-          </Link>
+        <div className="items-center gap-6">
+          {session ? (
+            <Link href={PageRoutes.DASHBOARD}>
+              <Button variant="default" className="text-sm">
+                Dashboard
+              </Button>
+            </Link>
+          ) : (
+            <>
+              <TextLink
+                href={PageRoutes.LOGIN}
+                className="hidden font-bold decoration-4 hover:underline md:flex"
+              >
+                Login
+              </TextLink>
+              <Link href={PageRoutes.SIGNUP}>
+                <Button variant="default" className="text-sm">
+                  Get Started
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
